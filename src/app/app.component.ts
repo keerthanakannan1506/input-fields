d84d6369400855819971f928b1fb37e29f473249
keerthanakannan1506 <keerthanakarthik1506@gmail.com>
2021-01-25 17:50:12 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  public name1:any
-  name = new FormControl('');
+export class AppComponent {
+  public name1: any;
+  public fN: any;
+  public lN: any;
+  name = new FormControl("");
 
-updateName() {
- this.name1 = this.name.value;
-}
+  gropingForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl("")
+  });
+
+  updateName() {
+    this.name1 = this.name.value;
+  }
+  updateFormgroup() {
+    this.fN = this.gropingForm.controls["firstName"].value;
+    this.lN = this.gropingForm.controls["lastName"].value;
+  }
 }
